16612819f8802c414d7ddb1b38fe090196ae7424
Haechi <combuff@nethru.co.kr>
2020-08-23 22:07:18 +0900

    플레이어 객체로부터 초기화면 구성

--- js/main.js
+++ js/main.js
@@ -1,9 +1,36 @@
+var TOTAL_SEATS = 9;
+
+var players = [
+	{
+		id: 0,
+		avatar: 1,
+		seat: 4,
+		name: "해치",
+		chip: 30000
+	},
+	{
+		id: 1,
+		avatar: 3,
+		seat: 1,
+		name: "장삐쭈",
+		chip: 15000
+	},
+	{
+		id: 2,
+		avatar: 11,
+		seat: 8,
+		name: "김아빠",
+		chip: 99999
+	}
+];
+
 document.addEventListener("DOMContentLoaded", function() {
 	window.addEventListener('resize', function() {
 		resize();
 	});
 	
-	resize();
+	resize();	
+	init();
 });
 
 function resize() {
@@ -18,4 +45,93 @@ function resize() {
 	document.body.style.transform = "scale(" + Math.min(ratioX, ratioY) + ")";
 	document.getElementById("table").style.transform = "translate(0px, " + (ratioY > 1 ? -offsetY : 0) + "px)";
 	window.scrollTo(0, offsetY);
+}
+
+function init() {
+	initTable();
+	initPlayers();
+}
+
+function initTable() {
+	showPlayers(false);
+	showPlayerChips(false);
+	showPlayerCards(false);
+	showPlayerTimers(false);
+	showPlayerDealerButtons(false);
+	
+	showFloorCards(false);
+	showFloorChip(false);
+}
+
+function initPlayers() {
+	for(var i = 0; i < players.length; i++) {
+		var player = players[i];
+		
+		updatePlayer(player);
+		showPlayer(player.seat, true);
+	}
+}
+
+function updatePlayer(player) {
+	document.querySelector("#p" + player.seat + " .avatar").setAttribute( 'src', 'image/avatar/avatar' + player.avatar + '.png' );
+	document.querySelector("#p" + player.seat + " .nickname").innerHTML = player.name;
+	document.querySelector("#p" + player.seat + " .chips").innerHTML = player.chip.toLocaleString();
+}
+
+function showPlayer(seat, visible) {	
+	document.querySelector("#p" + seat).style.display = visible ? "flex" : "none";
+}
+
+function showPlayers(visible) {
+	for(var seat = 0; seat < TOTAL_SEATS; seat++) {
+		showPlayer(seat, visible);
+	}
+}
+
+function showDealerbutton(seat, visible) {
+	document.querySelector("#p" + seat + " .dealerbutton").style.display = visible ? "flex" : "none";
+}
+
+function showPlayerDealerButtons(visible) {
+	for(var seat = 0; seat < TOTAL_SEATS; seat++) {
+		showDealerbutton(seat, visible);
+	}
+}
+
+function showPlayerChip(seat, visible) {
+	document.querySelector("#p" + seat + " .chip-container").style.display = visible ? "flex" : "none";
+}
+
+function showPlayerChips(visible) {
+	for(var seat = 0; seat < TOTAL_SEATS; seat++) {
+		showPlayerChip(seat, visible);
+	}
+}
+
+function showHoldingCard(seat, visible) {
+	document.querySelector("#p" + seat + " .holding-cards").style.display = visible ? "flex" : "none";
+}
+
+function showPlayerCards(visible) {
+	for(var seat = 0; seat < TOTAL_SEATS; seat++) {
+		showHoldingCard(seat, visible);
+	}
+}
+
+function showPlayerTimer(seat, visible) {
+	document.querySelector("#p" + seat + " .timer").style.display = visible ? "block" : "none";
+}
+
+function showPlayerTimers(visible) {
+	for(var seat = 0; seat < TOTAL_SEATS; seat++) {
+		showPlayerTimer(seat, visible);
+	}
+}
+
+function showFloorCards(visible) {
+	document.querySelector("#cards-on-floor").style.display = visible ? "flex" : "none";
+}
+
+function showFloorChip(visible) {
+	document.querySelector("#total-chip-container").style.display = visible ? "flex" : "none";
 }
