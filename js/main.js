5e8d9bc3bdbfab86d09d07e10ece9d9382e692be
Haechi <combuff@nethru.co.kr>
2020-08-27 22:40:12 +0900

    mock 데이터로 모든 상태의 화면 구성 - 플레이어 기본 정보 - 플레이어 카드 - 플레이어 칩

--- js/main.js
+++ js/main.js
@@ -2,47 +2,153 @@ var TOTAL_SEATS = 9;
 var CARDS_DEALING_INTERVAL = 100;
 var BETTING_TIMEOUT = 8;
 
+// 에러 코드
+var CODE_SUCCESS = 0;
+var CODE_NOT_YOUR_TURN = 1;
+
+// 플레이어 상태
+var PLAYER_PLAYING = 0;
+var PLAYER_SITTING_OUT = 1;
+
+// 게임 상태
+var GAME_BETTING = 0;
+var GAME_PRE_FLOP = 1;
+var GAME_FLOP = 2;
+var GAME_TURN = 3;
+var GAME_RIVER = 4;
+var GAME_RESULT = 5;
+
+// 카드 모양
+var SPADE = 0;
+var DIAMOND = 1;
+var HEART = 2;
+var CLUB = 3;
+
 var game = {
+	code: CODE_SUCCESS,
+	status: GAME_RIVER,
+	dealer: 3,
+	actor: 2,
+	floor: {
+		pot: 300,
+		cards: [
+			{
+				shape: CLUB,
+				number: 14
+			},
+			{
+				shape: HEART,
+				number: 13
+			},
+			{
+				shape: HEART,
+				number: 4
+			},
+			{
+				shape: DIAMOND,
+				number: 8
+			}
+		]
+	},
+	betting: {
+		min: 100,
+		call: 200
+	},
 	players: [
 		{
 			id: 0,
 			avatar: 1,
 			seat: 6,
 			name: "해치",
-			chip: 30000
+			chip: 30000,
+			betting: 100,
+			cards: [
+				{
+					shape: SPADE,
+					number: 0
+				},
+				{
+					shape: HEART,
+					number: 0
+				}
+			],
+			status: PLAYER_PLAYING
 		},
 		{
 			id: 1,
 			avatar: 3,
 			seat: 2,
 			name: "장삐쭈",
-			chip: 15000
+			chip: 15000,
+			cards: [
+				{
+					shape: SPADE,
+					number: 0
+				},
+				{
+					shape: HEART,
+					number: 0
+				}
+			],
+			status: PLAYER_PLAYING
 		},
 		{
 			id: 2,
 			avatar: 9,
 			seat: 8,
 			name: "삐쭈",
-			chip: 15000000
+			chip: 15000000,
+			cards: [
+				{
+					shape: SPADE,
+					number: 0
+				},
+				{
+					shape: HEART,
+					number: 0
+				}
+			],
+			status: PLAYER_PLAYING
 		},
 		{
 			id: 3,
 			avatar: 11,
 			seat: 4,
 			name: "김아빠",
-			chip: 99999
+			chip: 99999,
+			cards: [
+				{
+					shape: SPADE,
+					number: 14
+				},
+				{
+					shape: SPADE,
+					number: 13
+				}
+			],
+			status: PLAYER_PLAYING
 		},
 		{
 			id: 4,
 			avatar: 11,
 			seat: 7,
 			name: "임뽕구",
-			chip: 1500000
+			chip: 1500000,
+			betting: 200,
+			cards: [
+				{
+					shape: SPADE,
+					number: 0
+				},
+				{
+					shape: HEART,
+					number: 0
+				}
+			],
+			status: PLAYER_PLAYING
 		}
 	]
 };
-
-
 
 var dealerIndex = 0;
 var turn;
@@ -50,8 +156,8 @@ var bettingTimer;
 
 document.addEventListener("DOMContentLoaded", function() {
 	init();
-	resetTable();
 	bindEvents();
+	updateTable();
 });
 
 function nextGame() {
@@ -75,6 +181,79 @@ function resetTable() {
 	startBettingTimer();
 }
 
+// ----------------------------------------
+
+function updateTable() {
+	updatePlayers();
+}
+
+function updatePlayers() {
+	var players = game.players;
+	
+	for(var i = 0; i < players.length; i++) {
+		var player = players[i];
+		
+		updatePlayer(player);
+		updatePlayerCard(player, 1);
+		updatePlayerCard(player, 2);
+		
+		showPlayer(player.seat, true);
+		showPlayerCards(player.seat, true);
+		showPlayerChip(player.seat, player.betting > 0, player.betting);
+	}
+}
+
+function updatePlayer(player) {
+	document.querySelector(".seat" + player.seat + " .avatar").setAttribute( 'src', 'image/avatar/avatar' + player.avatar + '.png' );
+	document.querySelector(".seat" + player.seat + " .nickname").innerHTML = player.name;
+	document.querySelector(".seat" + player.seat + " .chips").innerHTML = player.chip.toLocaleString();
+}
+
+function updatePlayerCard(player, number) {
+	document.querySelector(".seat" + player.seat + " .card" + number).setAttribute("src", getCardImage(player, number));
+}
+
+function getCardImage(player, number) {
+	var index = number - 1;
+	
+	if(index < 0 || index >= player.cards.length) return "";
+	
+	var card = player.cards[index];
+	
+	return 'image/' + cardFile(card.number, card.shape);
+	
+	function cardFile(number, shape) {
+		if(number > 0) return cardNumber(number) + '_of_' + cardShape(shape) + '.png';
+		return 'back.png';
+	}
+	
+	function cardNumber(number) {
+		if(number == 11) return "jack";
+		else if(number == 12) return "queen";
+		else if(number == 13) return "king";
+		else if(number == 14) return "ace";
+		else return number;
+	}
+
+	function cardShape(shape) {
+		if(shape == 0) return "spades";
+		else if(shape == 1) return "diamonds";
+		else if(shape == 2) return "hearts";
+		else if(shape == 3) return "clubs";
+	}
+}
+
+function getNumber(number) {
+	if(number == 11) return "jack";
+	else if(number == 12) return "queen";
+	else if(number == 13) return "king";
+	else if(number == 14) return "ace";
+	
+	return number;
+}
+
+// ----------------------------------------
+
 function passDealerButton() {
 	showAllPlayerDealerButtons(false);
 	showPlayerDealerButton(game.players[dealerIndex].seat, true);
@@ -85,9 +264,7 @@ function passDealerButton() {
 function init() {
 	resize();
 	initTable();
-	initPlayers();
 	sortPlayersBySeat();
-	firstTurn();
 }
 
 function initTable() {
@@ -99,17 +276,6 @@ function initTable() {
 	
 	showFloorCards(false);
 	showFloorChip(false);
-}
-
-function initPlayers() {
-	var players = game.players;
-	
-	for(var i = 0; i < players.length; i++) {
-		var player = players[i];
-		
-		updatePlayer(player);
-		showPlayer(player.seat, true);
-	}
 }
 
 // ----------------------------------------
@@ -213,12 +379,6 @@ function showBettingTimer() {
 
 // ----------------------------------------
 
-function updatePlayer(player) {
-	document.querySelector(".seat" + player.seat + " .avatar").setAttribute( 'src', 'image/avatar/avatar' + player.avatar + '.png' );
-	document.querySelector(".seat" + player.seat + " .nickname").innerHTML = player.name;
-	document.querySelector(".seat" + player.seat + " .chips").innerHTML = player.chip.toLocaleString();
-}
-
 function showPlayer(seat, visible) {
 	document.querySelector(".seat" + seat).style.display = visible ? "flex" : "none";
 }
@@ -231,7 +391,8 @@ function showPlayerTimer(seat, visible) {
 	document.querySelector(".seat" + seat + " .timer").style.display = visible ? "block" : "none";
 }
 
-function showPlayerChip(seat, visible) {
+function showPlayerChip(seat, visible, amount) {
+	if(amount) document.querySelector(".seat" + seat + " .count").innerHTML = amount;
 	document.querySelector(".seat" + seat + " .chip-container").style.display = visible ? "flex" : "none";
 }
 
