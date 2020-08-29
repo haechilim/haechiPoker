3f4a5c09605a14e299316a257611dfdf29677718
Haechi <combuff@nethru.co.kr>
2020-08-29 18:46:52 +0900

    mime패키지 적용

--- js/server.js
+++ js/server.js
@@ -1,15 +1,16 @@
 var http = require("http");
 var fs = require("fs");
+var mime = require("mime");
 
-var server = http.createServer(function(request, response) {
-	var path = request.url == "/" ? "poker.html" : request.url.substr(1);
+var server = http.createServer(function(request, response) {	
+	var path = request.url == "/" ? "poker.html" : request.url.substr(1);	
 	
 	if(isBinaryFile(path)) fs.readFile(path, handleFile);
 	else fs.readFile(path, "UTF-8", handleFile);
 	
 	function handleFile(error, data) {
 		response.writeHead(200, {
-			"Content-Type": getContentType(path)
+			"Content-Type": mime.getType(path)
 		});
 		
 		response.end(data);
@@ -19,31 +20,6 @@ var server = http.createServer(function(request, response) {
 server.listen(8888);
 
 console.log("나 듣고 있다!");
-
-function getContentType(url) {
-	var index = url.lastIndexOf(".");
-	
-	if (index < 0 || index >= url.length) return "";
-	
-	var extension = url.substr(index + 1);
-	
-	switch(extension) {
-		case "html":
-		case "css":
-			return "text/" + extension + "; charset=utf-8";
-			
-		case "png":
-		case "jpg":
-		case "gif":
-			return "image/" + extension;
-			
-		case "ico":
-			return "image/x-icon";
-			
-		case "js":
-			return "application/javascript" + "; charset=utf-8";
-	}
-}
 
 function isBinaryFile(url) {
 	var extensions = [ ".html", ".css", ".js" ];
